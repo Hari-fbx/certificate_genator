087d70e2e78f654cc76243c32dc3d6ab05cefe85
Hari Krishna <harikrishnamohan82@gmail.com>
2021-11-20 16:49:47 +0530

    v1

--- generator.py
+++ generator.py
@@ -2,13 +2,18 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 import os
-img = Image.open('webinartemplate_withoutname.png')
-data="Hari Krishna M"
-orgin=(2049 ,2442)
-font_size=180
+image_path="certificates/webinartemplate.png"
+img = Image.open(image_path)
+names= os.environ.get("PARTICIPANTS").split(',')
+orgin=(2334/2 ,920)
+font_size=90
 font_color=(0,0,0)
-drawer = ImageDraw.Draw(img)
-font = ImageFont.truetype('fonts\Montserrat-Medium.ttf', font_size)
-drawer.text(orgin, data, font=font, fill = font_color)
-path=str("certificates/"+data+".png")
-img.save(path)
+font = ImageFont.truetype('fonts\Montserrat-Medium.ttf', int(font_size*1.33333))
+print(names)
+for data in names:
+    img = Image.open(image_path)
+    drawer = ImageDraw.Draw(img)
+    drawer.text(orgin, data, font=font, fill = font_color,anchor="ms")
+    path=str("certificates/"+data.replace("."," ")+".png")
+    print(path)
+    img.save(path)
